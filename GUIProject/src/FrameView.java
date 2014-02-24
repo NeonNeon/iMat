a7376b2dcb0493d6940108ce6c29806874900ac9
Neon <e.olof.norlander@gmail.com>
2014-02-24 17:58:27 +0100

    fixing the project folder, adding gitignore

---
+++ GUIProject/src/FrameView.java
@@ -0,0 +1,49 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+
+public class FrameView extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrameView frame = new FrameView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrameView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(70, 50, 1200, 680);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(0, 0, 250, 50);
+		contentPane.add(btnNewButton);
+		
+		JButton button = new JButton("New button");
+		button.setBounds(this.getWidth()-250, 0, 250, 50);
+		contentPane.add(button);
+	}
+}
