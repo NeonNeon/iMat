d8c500158ebdded144958f6bb578a28b89153569
MatildaHorppu <matilda.horppu@live.se>
2014-02-24 16:05:33 +0100

    frame size set

--- iMatProjekt/src/FrameView.java
+++ iMatProjekt/src/FrameView.java
@@ -31,7 +31,7 @@ public class FrameView extends JFrame {
 	 */
 	public FrameView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(70, 50, 1200, 680);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
