ee5e9b3d09ab0b9e1025ff20cff5ae4942495aa4
Neon <e.olof.norlander@gmail.com>
2014-02-25 09:42:18 +0100

    Added the CartView class

---
+++ GUIProject/src/CartView.java
@@ -0,0 +1,14 @@
+import javax.swing.JPanel;
+
+
+public class CartView extends JPanel {
+private static final int WIDTH = 250;
+private static final int HEIGHT = 681;
+	/**
+	 * Create the panel.
+	 */
+	public CartView() {
+		setSize(WIDTH, HEIGHT);
+	}
+
+}
