3adb7fd8edfd178d3ee510569e56d53d2f0b7cc7
Neon <e.olof.norlander@gmail.com>
2014-02-25 10:17:48 +0100

    Added a panel for the nameLabel

--- GUIProject/src/CartView.java
+++ GUIProject/src/CartView.java
@@ -10,6 +10,7 @@ import javax.swing.SwingConstants;
 public class CartView extends JPanel {
 private static final int WIDTH = 250;
 private static final int HEIGHT = 681;
+private static final int NAME_PANEL_HEIGHT = 100;
 
 	/**
 	 * Create the panel.
@@ -17,11 +18,13 @@ private static final int HEIGHT = 681;
 	public CartView() {
 		setSize(WIDTH, HEIGHT);
 		setLayout(new BorderLayout(0, 0));
-		
+		JPanel namePanel = new JPanel();
+		namePanel.setSize(WIDTH, NAME_PANEL_HEIGHT);
+		add(namePanel,BorderLayout.NORTH);
 		JLabel nameLabel = new JLabel("Stefan Svantesson");
 		nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		nameLabel.setFont(new Font("DejaVu Sans", Font.BOLD, 20));
-		add(nameLabel,BorderLayout.NORTH);
+		namePanel.add(nameLabel);
 		
 		JPanel cartPanel = new JPanel();
 		cartPanel.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
