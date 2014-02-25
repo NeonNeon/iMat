6a67f248a631928a1db13e2146d9f8c5c538e8e0
Neon <e.olof.norlander@gmail.com>
2014-02-25 21:53:35 +0100

    Added getters and setters for the profilename

--- GUIProject/src/CartView.java
+++ GUIProject/src/CartView.java
@@ -19,10 +19,10 @@ public class CartView extends JPanel {
 private static final int WIDTH = 250;
 private static final int HEIGHT = 681;
 private static final int NAME_PANEL_HEIGHT = 100;
-
+private JLabel nameLabel;
 	/**
 	 * Create the panel.
-	 */
+	 */	
 	public CartView() {
 		setSize(WIDTH, HEIGHT);
 		SpringLayout springLayout = new SpringLayout();
@@ -33,16 +33,18 @@ private static final int NAME_PANEL_HEIGHT = 100;
 		springLayout.putConstraint(SpringLayout.SOUTH, namePanel, 100, SpringLayout.NORTH, this);
 		springLayout.putConstraint(SpringLayout.EAST, namePanel, 0, SpringLayout.EAST, this);
 		namePanel.setSize(WIDTH, NAME_PANEL_HEIGHT);
-		add(namePanel,BorderLayout.CENTER);
+		add(namePanel);
 		
 		JPanel cartPanel = new JPanel();
 		SpringLayout sl_cartPanel = new SpringLayout();
 		cartPanel.setLayout(sl_cartPanel);
 		springLayout.putConstraint(SpringLayout.NORTH, cartPanel, 0, SpringLayout.SOUTH, namePanel);
 		springLayout.putConstraint(SpringLayout.SOUTH, cartPanel, 600, SpringLayout.SOUTH, namePanel);
-		JLabel nameLabel = new JLabel("Stefan Svantesson");
+		nameLabel = new JLabel("Stefan Svantesson");
 		nameLabel.setSize(WIDTH, NAME_PANEL_HEIGHT);
 		namePanel.add(nameLabel);
+		
+		
 		springLayout.putConstraint(SpringLayout.WEST, nameLabel, 0, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.SOUTH, nameLabel, 0, SpringLayout.NORTH, cartPanel);
 		nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -50,16 +52,21 @@ private static final int NAME_PANEL_HEIGHT = 100;
 		springLayout.putConstraint(SpringLayout.WEST, cartPanel, 0, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.EAST, cartPanel, 0, SpringLayout.EAST, this);
 		cartPanel.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
-		add(cartPanel, BorderLayout.CENTER);
+		add(cartPanel);
 		
 		JLabel varukorgLabel = new JLabel("Varukorg");
-		varukorgLabel.setSize(WIDTH,NAME_PANEL_HEIGHT);
 		varukorgLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		varukorgLabel.setSize(WIDTH,NAME_PANEL_HEIGHT);
 		sl_cartPanel.putConstraint(SpringLayout.NORTH, varukorgLabel, 0, SpringLayout.NORTH, cartPanel);
 		sl_cartPanel.putConstraint(SpringLayout.WEST, varukorgLabel, 0, SpringLayout.WEST, cartPanel);
 		sl_cartPanel.putConstraint(SpringLayout.EAST, varukorgLabel, 0, SpringLayout.EAST, cartPanel);
 		varukorgLabel.setFont(new Font("Dialog", Font.BOLD, 22));
 		cartPanel.add(varukorgLabel);
 	}
-
+	public void setName(String name) {
+		nameLabel.setText(name);
+	}
+	public String getName() {
+		return nameLabel.getText();
+	}
 }
