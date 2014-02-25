0cc8d88d0b577815456b834fd2d9e5c120c8d81b
Neon <e.olof.norlander@gmail.com>
2014-02-25 09:55:57 +0100

    Added nameLabel

--- GUIProject/src/CartView.java
+++ GUIProject/src/CartView.java
@@ -1,14 +1,21 @@
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 
 public class CartView extends JPanel {
 private static final int WIDTH = 250;
 private static final int HEIGHT = 681;
+
 	/**
 	 * Create the panel.
 	 */
 	public CartView() {
 		setSize(WIDTH, HEIGHT);
+		
+		JLabel nameLabel = new JLabel("Stefan Svantesson");
+		nameLabel.setFont(new Font("DejaVu Sans", Font.BOLD, 20));
+		add(nameLabel);
 	}
 
 }
