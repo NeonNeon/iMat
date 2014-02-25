29c6aa41b5d9957aa1800fcce728a5c93bab69f9
Neon <e.olof.norlander@gmail.com>
2014-02-25 10:03:52 +0100

    Added a panel with a border for the cart

--- GUIProject/src/CartView.java
+++ GUIProject/src/CartView.java
@@ -2,6 +2,9 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.BorderLayout;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.SwingConstants;
 
 
 public class CartView extends JPanel {
@@ -16,8 +19,13 @@ private static final int HEIGHT = 681;
 		setLayout(new BorderLayout(0, 0));
 		
 		JLabel nameLabel = new JLabel("Stefan Svantesson");
+		nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		nameLabel.setFont(new Font("DejaVu Sans", Font.BOLD, 20));
 		add(nameLabel,BorderLayout.NORTH);
+		
+		JPanel cartPanel = new JPanel();
+		cartPanel.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		add(cartPanel, BorderLayout.CENTER);
 	}
 
 }
