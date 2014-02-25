158134a2a9e78d0db6e1cf6d3cdf721173373c5f
lovisajaberg <jaberglovisa@gmail.com>
2014-02-25 22:44:09 +0100

    Still working on this one, bro

--- GUIProject/src/SearchView.java
+++ GUIProject/src/SearchView.java
@@ -10,24 +10,53 @@ import java.awt.GridLayout;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JSeparator;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JToolBar;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class SearchView extends JPanel {
 	private JTextField textField;
+	private Color originalColor = new Color(255, 102, 51);
+	private Color hoverColor = new Color(255,182,193);
+	private Font categoryFont = new Font("Gill Sans", Font.PLAIN, 20);
 
+	private MouseAdapter myMouseListener = new MouseAdapter(){
+			@Override
+			public void mouseEntered(MouseEvent evt) {
+				((JLabel)evt.getSource()).setForeground(hoverColor);
+			}
+			@Override
+			public void mouseExited(MouseEvent evt) {
+				((JLabel)evt.getSource()).setForeground(originalColor);
+			}
+	};
+	
 	/**
 	 * Create the panel.
 	 */
 	public SearchView() {
+		setBackground(new Color(255, 255, 255));
 		setSize(250,681);
 		setLayout(null);
 
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(0, 0, 250, 100);
-		add(btnNewButton);
+		JButton logoButton = new JButton("iMat");
+		logoButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		logoButton.setFont(new Font("Gill Sans", Font.PLAIN, 50));
+		logoButton.setForeground(new Color(255, 69, 0));
+		logoButton.setBackground(new Color(255, 182, 193));
+		logoButton.setBounds(0, 0, 250, 100);
+		add(logoButton);
 
 		textField = new JTextField();
-		textField.setBounds(0, 112, 250, 42);
+		textField.setBounds(10, 112, 222, 42);
 		add(textField);
 		textField.setColumns(10);
 		textField.setText("Sök..");
@@ -53,93 +82,130 @@ public class SearchView extends JPanel {
 		 */
 
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 180, 250, 337);
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(20, 175, 212, 330);
 		add(panel);
-		panel.setLayout(new GridLayout(19, 1, 0, 0));
+		panel.setLayout(new GridLayout(14, 1, 9, 0));
 
 
 		JLabel favoritesLabel = new JLabel("Favoriter");
+		favoritesLabel.addMouseListener(myMouseListener);
+		favoritesLabel.setForeground(originalColor);
+		favoritesLabel.setBackground(hoverColor);
 		favoritesLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		panel.add(favoritesLabel);
-		favoritesLabel.setFont(new Font("Gill Sans", Font.BOLD, 22));
+		favoritesLabel.setFont(categoryFont);
 		favoritesLabel.setSize(681,30);
-		
-		JSeparator separator = new JSeparator();
-		panel.add(separator);
 
 		JLabel greensLabel = new JLabel("Grönsaker");
+		greensLabel.addMouseListener(myMouseListener);
+		greensLabel.setForeground(originalColor);
+		greensLabel.setBackground(hoverColor);
 		greensLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		greensLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		greensLabel.setFont(categoryFont);
 		panel.add(greensLabel);
 
 
 		JLabel fruitLabel = new JLabel("Frukt och bär");
+		fruitLabel.addMouseListener(myMouseListener);
+		fruitLabel.setForeground(originalColor);
+		fruitLabel.setBackground(hoverColor);
 		fruitLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		fruitLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		fruitLabel.setFont(categoryFont);
 		panel.add(fruitLabel);
 
 
 		JLabel herbLabel = new JLabel("Örter");
+		herbLabel.addMouseListener(myMouseListener);
+		herbLabel.setForeground(originalColor);
+		herbLabel.setBackground(hoverColor);
 		herbLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		herbLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		herbLabel.setFont(categoryFont);
 		panel.add(herbLabel);
 
 		JLabel leguminousLabel = new JLabel("Baljväxter");
+		leguminousLabel.addMouseListener(myMouseListener);
+		leguminousLabel.setForeground(originalColor);
+		leguminousLabel.setBackground(hoverColor);
 		leguminousLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		leguminousLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		leguminousLabel.setFont(categoryFont);
 		panel.add(leguminousLabel);
 
 
 		JLabel rootvegLabel = new JLabel("Potatis och rotfrukter");
+		rootvegLabel.addMouseListener(myMouseListener);
+		rootvegLabel.setForeground(originalColor);
+		rootvegLabel.setBackground(hoverColor);
 		rootvegLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		rootvegLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		rootvegLabel.setFont(categoryFont);
 		panel.add(rootvegLabel);
 
 		JLabel fishLabel = new JLabel("Fisk och skaldjur");
+		fishLabel.addMouseListener(myMouseListener);
+		fishLabel.setForeground(originalColor);
+		fishLabel.setBackground(hoverColor);
 		fishLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		fishLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		fishLabel.setFont(categoryFont);
 		panel.add(fishLabel);
 
 		JLabel meatLabel = new JLabel("Kött");
+		meatLabel.addMouseListener(myMouseListener);
+		meatLabel.setForeground(originalColor);
+		meatLabel.setBackground(hoverColor);
 		meatLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		meatLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		meatLabel.setFont(categoryFont);
 		panel.add(meatLabel);
 
 		JLabel breadLabel = new JLabel("Bröd");
+		breadLabel.addMouseListener(myMouseListener);
+		breadLabel.setForeground(originalColor);
+		breadLabel.setBackground(hoverColor);
 		breadLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		breadLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		breadLabel.setFont(categoryFont);
 		panel.add(breadLabel);
 
 		JLabel dairyLabel = new JLabel("Mejeri");
+		dairyLabel.addMouseListener(myMouseListener);
+		dairyLabel.setForeground(originalColor);
+		dairyLabel.setBackground(hoverColor);
 		dairyLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		dairyLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		dairyLabel.setFont(categoryFont);
 		panel.add(dairyLabel);
 
 		JLabel riceLabel = new JLabel("Ris och Pasta");
+		riceLabel.addMouseListener(myMouseListener);
+		riceLabel.setForeground(originalColor);
+		riceLabel.setBackground(hoverColor);
 		riceLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		riceLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		riceLabel.setFont(categoryFont);
 		panel.add(riceLabel);
 
 		JLabel pantryLabel = new JLabel("Skafferi");
+		pantryLabel.addMouseListener(myMouseListener);
+		pantryLabel.setForeground(originalColor);
+		pantryLabel.setBackground(hoverColor);
 		pantryLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		pantryLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		pantryLabel.setFont(categoryFont);
 		panel.add(pantryLabel);
 
 		JLabel drinksLabel = new JLabel("Dryck");
+		drinksLabel.addMouseListener(myMouseListener);
+		drinksLabel.setForeground(originalColor);
+		drinksLabel.setBackground(hoverColor);
 		drinksLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		drinksLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		drinksLabel.setFont(categoryFont);
 		panel.add(drinksLabel);
 
 
 
 		JLabel snackLabel = new JLabel("Snacks och sötsaker");
+		snackLabel.addMouseListener(myMouseListener);
+		snackLabel.setForeground(originalColor);
+		snackLabel.setBackground(hoverColor);
 		snackLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		snackLabel.setFont(new Font("Gill Sans", Font.BOLD, 14));
+		snackLabel.setFont(categoryFont);
 		panel.add(snackLabel);
 
 
-
 	}
-
-	//TODO - iMat-knapp sökfält kategorilista
 }
