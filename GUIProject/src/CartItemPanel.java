1671a6445ad88a33f50db79b456ebd940cb4f403
Neon <e.olof.norlander@gmail.com>
2014-02-26 11:49:15 +0100

    added the correct size(?)

--- GUIProject/src/CartItemPanel.java
+++ GUIProject/src/CartItemPanel.java
@@ -3,19 +3,25 @@ import javax.swing.JPanel;
 import se.chalmers.ait.dat215.project.ShoppingItem;
 
 /**
- * The small panel that lies within the CartView.
- * Each CartItemPanel has a ShoppingItem as an instance variable
- * <p> and updates it when update() is called.
+ * The small panel that lies within the CartView. Each CartItemPanel has a
+ * ShoppingItem as an instance variable
+ * <p>
+ * and updates it when update() is called.
+ * 
  * @author neon
- *
+ * 
  */
 public class CartItemPanel extends JPanel {
-private ShoppingItem item;
+	private static final int WIDTH = 230;
+	private static final int HEIGHT = 50;
+	private ShoppingItem item;
+
 	/**
 	 * Create the panel.
 	 */
 	public CartItemPanel(ShoppingItem item) {
 		this.item = item;
+		setSize(WIDTH, HEIGHT);
 	}
 
 }
