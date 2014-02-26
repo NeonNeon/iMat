656038d427df2aa3ec92e5e945c68a1e6b8333f6
Neon <e.olof.norlander@gmail.com>
2014-02-26 11:41:59 +0100

    Added javadoc to CartItemPanel

--- GUIProject/src/CartItemPanel.java
+++ GUIProject/src/CartItemPanel.java
@@ -1,13 +1,21 @@
 import javax.swing.JPanel;
 
+import se.chalmers.ait.dat215.project.ShoppingItem;
 
+/**
+ * The small panel that lies within the CartView.
+ * Each CartItemPanel has a ShoppingItem as an instance variable
+ * <p> and updates it when update() is called.
+ * @author neon
+ *
+ */
 public class CartItemPanel extends JPanel {
-
+private ShoppingItem item;
 	/**
 	 * Create the panel.
 	 */
-	public CartItemPanel() {
-
+	public CartItemPanel(ShoppingItem item) {
+		this.item = item;
 	}
 
 }
