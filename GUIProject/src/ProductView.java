88e5b7ff6e8200decf4c131e7cadedc9d672f5db
Jesper Olsson <jeppzone@gmail.com>
2014-02-26 13:18:08 +0100

    Added some functionality to productView and CategoryDetailedView

--- GUIProject/src/ProductView.java
+++ GUIProject/src/ProductView.java
@@ -1,34 +1,45 @@
 import javax.swing.JPanel;
+
+import se.chalmers.ait.dat215.project.*;
+import se.chalmers.ait.dat215.project.util.*;
+
 import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JSpinner;
 import javax.swing.JButton;
+
 import java.awt.Font;
+
 import javax.swing.border.LineBorder;
 
-
 public class ProductView extends JPanel {
-
+	private static final Model model = Model.getInstance();
+	private JButton favouriteButton, addToListButton, addToCartButton;
+	
 	/**
 	 * Create the panel.
 	 */
-	public ProductView() {
+	public ProductView(Product p) {
+		
 		setBorder(new LineBorder(new Color(204, 255, 153), 1, true));
 		setBackground(new Color(255, 255, 240));
 		
 		setSize(170, 216);
 		setLayout(null);
 		
-		JLabel pictureLabel = new JLabel("New label");
+		JLabel pictureLabel = new JLabel(model.getImageIcon(p,20,20));
 		pictureLabel.setBounds(10, 11, 150, 109);
 		add(pictureLabel);
 		
-		JLabel nameLabel = new JLabel("New label");
+		JLabel nameLabel = new JLabel(p.getName());
 		nameLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		nameLabel.setBounds(10, 131, 116, 14);
 		add(nameLabel);
 		
-		JLabel priceLabel = new JLabel("New label");
+		JLabel priceLabel = new JLabel(p.getPrice()+" kr/"+p.getUnitSuffix());
 		priceLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		priceLabel.setBounds(10, 160, 116, 14);
 		add(priceLabel);
@@ -37,22 +48,22 @@ public class ProductView extends JPanel {
 		spinner.setBounds(10, 185, 40, 20);
 		add(spinner);
 		
-		JLabel lblSt = new JLabel("st");
-		lblSt.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblSt.setBounds(51, 187, 46, 14);
-		add(lblSt);
+//		JLabel lblSt = new JLabel("st");
+//		lblSt.setFont(new Font("Tahoma", Font.PLAIN, 12));
+//		lblSt.setBounds(51, 187, 46, 14);
+//		add(lblSt);
 		
-		JButton favouriteButton = new JButton("New button");
+		favouriteButton = new JButton("New button");
 		favouriteButton.setToolTipText("L\u00E4gg till som favorit");
 		favouriteButton.setBounds(136, 127, 24, 23);
 		add(favouriteButton);
 		
-		JButton addToListButton = new JButton("New button");
+		addToListButton = new JButton("New button");
 		addToListButton.setToolTipText("L\u00E4gg till i lista");
 		addToListButton.setBounds(136, 156, 24, 23);
 		add(addToListButton);
 		
-		JButton addToCartButton = new JButton("New button");
+		addToCartButton = new JButton("New button");
 		addToCartButton.setToolTipText("L\u00E4gg till i varukorg");
 		addToCartButton.setBounds(136, 184, 24, 23);
 		add(addToCartButton);
