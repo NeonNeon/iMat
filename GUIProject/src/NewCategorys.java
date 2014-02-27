1e1b89a839b67c1594c867a139066d6749cf8880
Neon <e.olof.norlander@gmail.com>
2014-02-27 01:10:09 +0100

    added a new enum NewCategorys

---
+++ GUIProject/src/NewCategorys.java
@@ -0,0 +1,16 @@
+/**
+ * This is an enum that we should use instead of the ProductCategory enum.
+ * 
+ * @author neon
+ * 
+ */
+
+/*
+ * Kategorier: - favoriter - gr�nsaker - frukt och b�r - baljv�xter - potatis
+ * och rotfrukter - ris och pasta - skafferi - br�d - mejeriprodukter - k�tt -
+ * fisk och skaldjur - dryck - �rter -snacks och s�tsaker
+ */
+public enum NewCategorys {
+	FAVORITER,GRONSAKER,FRUKT_OCH_BAR,BALJVAXTER,POTATIS_OCH_ROTFRUKTER,RIS_OCH_PASTA,SKAFFERI,
+	BROD,MEJERI,KOTT,FISK_OCH_SKALDJUR,DRYCK,ORTER,SNACKS_OCH_SOTSAKER;
+}
