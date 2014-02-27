6c68a92b9ef1afcf31807606016b594fe16ca5d5
Jesper Olsson <jeppzone@gmail.com>
2014-02-27 19:28:44 +0100

    Layout in CategoryDetailedView and ProductView fixed

--- GUIProject/src/CategoryDetailedView.java
+++ GUIProject/src/CategoryDetailedView.java
@@ -1,6 +1,7 @@
 import javax.swing.JPanel;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
@@ -11,7 +12,10 @@ import se.chalmers.ait.dat215.project.*;
 
 import java.awt.Font;
 import java.util.List;
+
 import javax.swing.JScrollPane;
+
+import static javax.swing.JScrollPane.*;
 
 /*
  * A class to present all the products in one specific category
@@ -19,36 +23,48 @@ import javax.swing.JScrollPane;
 public class CategoryDetailedView extends JPanel {
 	private static final Model model = Model.getInstance();
 	private List<Product> pl;
-	private int rows = 0;
+	private String name;
+	private double height;
+	private NewCategorys nc;
+	private JPanel panel;
 	/**
 	 * Create the panel.
 	 */
-	public CategoryDetailedView(ProductCategory pc) {
-		pl = model.getProducts(pc);
+	public CategoryDetailedView(String name, NewCategorys nc) {
+		this.name = name;
+		this.nc=nc;
+		pl = model.getProducts(nc);
+		
 		setBorder(new LineBorder(new Color(204, 255, 153), 4, true));
 		setBackground(new Color(255, 255, 240));
 		setSize(684, 681);
-		rows = pl.size()/4;
-		if(rows%4==0){
-			setLayout(new GridLayout(rows, 4));
-		}else{
-			setLayout(new GridLayout(rows+1, 4));
-		}
+		setLayout(null);
 		
-//		JLabel rubrikLabel = new JLabel("New label");
-//		rubrikLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-//		rubrikLabel.setBounds(270, 9, 128, 31);
-//		rubrikLabel.setText(pc.toString());
-//		add(rubrikLabel);
-//		
+		JLabel lblNewLabel = new JLabel(name, JLabel.CENTER);
+		lblNewLabel.setBounds(262, 13, 127, 39);
+		add(lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 65, 684, 616);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_ALWAYS);
+		add(scrollPane);
+		
+		height = (pl.size()/3)*300;
+		
+		panel = new JPanel();
+		panel.setLayout(new GridLayout(0,3));
+		panel.setPreferredSize(new Dimension(684,(int)height));
+		scrollPane.setViewportView(panel);
+		
+		addProducts();
+		
+	}
 	
-		//add all the products in category to productpanel
+	public void addProducts(){
 		for(int i = 0; i<pl.size(); i++){
-			add(new ProductView(pl.get(i)));
-			//System.out.println(pl.get(i).getName());
+			panel.add(new ProductView(pl.get(i)));
 			
 		}
-		
-		System.out.println(6/4);
 	}
 }
--- GUIProject/src/ProductView.java
+++ GUIProject/src/ProductView.java
@@ -4,6 +4,7 @@ import se.chalmers.ait.dat215.project.*;
 import se.chalmers.ait.dat215.project.util.*;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
@@ -34,25 +35,25 @@ public class ProductView extends JPanel {
 		setBorder(new LineBorder(new Color(204, 255, 153), 1, true));
 		setBackground(new Color(255, 255, 240));
 		
-		setSize(WIDTH, HEIGHT);
+		setPreferredSize(new Dimension(200, 260));
 		setLayout(null);
 		
-		JLabel pictureLabel = new JLabel(model.getImageIcon(p,100,100));
-		pictureLabel.setBounds(10, 13, 150, 93);
+		JLabel pictureLabel = new JLabel(model.getImageIcon(p,130,100));
+		pictureLabel.setBounds(46, 13, 130, 100);
 		add(pictureLabel);
 		
 		JLabel nameLabel = new JLabel(p.getName());
 		nameLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		nameLabel.setBounds(10, 117, 116, 20);
+		nameLabel.setBounds(10, 145, 116, 20);
 		add(nameLabel);
 		
 		JLabel priceLabel = new JLabel(p.getPrice()+" kr/"+p.getUnitSuffix());
 		priceLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		priceLabel.setBounds(10, 144, 116, 25);
+		priceLabel.setBounds(10, 178, 130, 25);
 		add(priceLabel);
 		
 		JSpinner spinner = new JSpinner();
-		spinner.setBounds(10, 182, 40, 20);
+		spinner.setBounds(10, 216, 38, 20);
 		add(spinner);
 		
 //		JLabel lblSt = new JLabel("st");
@@ -62,18 +63,18 @@ public class ProductView extends JPanel {
 		
 		favouriteButton = new JButton("New button");
 		favouriteButton.setToolTipText("L\u00E4gg till som favorit");
-		favouriteButton.setBounds(136, 119, 24, 23);
+		favouriteButton.setBounds(152, 145, 24, 23);
 
 		add(favouriteButton);
 		
 		addToListButton = new JButton("New button");
 		addToListButton.setToolTipText("L\u00E4gg till i lista");
-		addToListButton.setBounds(136, 146, 24, 23);
+		addToListButton.setBounds(152, 180, 24, 23);
 		add(addToListButton);
 		
 		addToCartButton = new JButton("New button");
 		addToCartButton.setToolTipText("L\u00E4gg till i varukorg");
-		addToCartButton.setBounds(130, 182, 30, 23);
+		addToCartButton.setBounds(152, 215, 24, 23);
 		add(addToCartButton);
 
 	}
