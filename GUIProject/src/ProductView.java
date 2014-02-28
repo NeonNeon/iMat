e176323beb8dba00f57553805aff168f985a1c3c
Jesper Olsson <jeppzone@gmail.com>
2014-02-28 10:35:13 +0100

    Some more changes to layout in categorydetailedview

--- GUIProject/src/ProductView.java
+++ GUIProject/src/ProductView.java
@@ -4,6 +4,7 @@ import se.chalmers.ait.dat215.project.*;
 import se.chalmers.ait.dat215.project.util.*;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 
@@ -16,6 +17,9 @@ import java.awt.Font;
 
 import javax.swing.border.LineBorder;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 
 /*
  * A class to represent an iMat  product
@@ -25,12 +29,22 @@ public class ProductView extends JPanel {
 	private static final int HEIGHT = 216;
 	private static final Model model = Model.getInstance();
 	private JButton favouriteButton, addToListButton, addToCartButton;
+
 	
 	
 	/**
 	 * Create the panel.
 	 */
 	public ProductView(Product p) {
+		MouseAdapter myMouseListener = new MouseAdapter(){
+			public void mouseEntered(MouseEvent e) {
+				((JButton)e.getSource()).setCursor(new Cursor(Cursor.HAND_CURSOR));
+					
+				
+			}
+
+		
+		};
 		
 		setBorder(new LineBorder(new Color(204, 255, 153), 1, true));
 		setBackground(new Color(255, 255, 240));
@@ -44,10 +58,12 @@ public class ProductView extends JPanel {
 		
 		JLabel nameLabel = new JLabel(p.getName());
 		nameLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		nameLabel.setBounds(10, 102, 116, 20);
+		nameLabel.setForeground((Color.BLACK));
+		nameLabel.setBounds(10, 102, 130, 20);
 		add(nameLabel);
 		
 		JLabel priceLabel = new JLabel(p.getPrice()+" kr/"+p.getUnitSuffix());
+		priceLabel.setForeground(Color.BLACK);
 		priceLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		priceLabel.setBounds(10, 135, 130, 25);
 		add(priceLabel);
@@ -57,18 +73,26 @@ public class ProductView extends JPanel {
 		add(spinner);
 		
 		favouriteButton = new JButton("New button");
+		favouriteButton.addMouseListener(myMouseListener);
+			
+		
 		favouriteButton.setToolTipText("L\u00E4gg till som favorit");
-		favouriteButton.setBounds(152, 102, 24, 23);
+		favouriteButton.setBounds(152, 102, 36, 23);
+		//favouriteButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		add(favouriteButton);
 		
 		addToListButton = new JButton("New button");
 		addToListButton.setToolTipText("L\u00E4gg till i lista");
-		addToListButton.setBounds(152, 137, 24, 23);
+		addToListButton.setBounds(152, 137, 36, 23);
+		//addToListButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		addToListButton.addMouseListener(myMouseListener);
 		add(addToListButton);
 		
 		addToCartButton = new JButton("New button");
 		addToCartButton.setToolTipText("L\u00E4gg till i varukorg");
-		addToCartButton.setBounds(152, 172, 24, 23);
+		addToCartButton.setBounds(152, 172, 36, 23);
+		//addToCartButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		addToCartButton.addMouseListener(myMouseListener);
 		add(addToCartButton);
 
 	}
