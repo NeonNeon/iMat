ecddfc78f837d46ce6d3bde02d13431834cf572b
lovisajaberg <jaberglovisa@gmail.com>
2014-03-04 13:11:18 +0100

    Colors and place

--- GUIProject/src/view/CheckOutView.java
+++ GUIProject/src/view/CheckOutView.java
@@ -75,93 +75,6 @@ public class CheckOutView extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(cardLayout);
 
-		JPanel card1 = new JPanel();
-		card1.setBackground(Constants.CONTRASTCOLOR.getColor());
-		contentPane.add(card1, "Card1");
-		card1.setLayout(null);
-
-		JSeparator separator_1 = new JSeparator();
-		separator_1.setBounds(350, 5, 10, 555);
-		separator_1.setOrientation(SwingConstants.VERTICAL);
-		card1.add(separator_1);
-		JScrollPane cartPane = new JScrollPane();
-		cartPane.setBounds(375, 20, 300, 455);
-		card1.add(cartPane);
-
-
-		JLabel deliveryDay1 = new JLabel("Leveransdag");
-		deliveryDay1.setBounds(25, 210, 80, 15);
-		card1.add(deliveryDay1);
-
-		JLabel deliveryAddress1 = new JLabel("Leveransadress");
-		deliveryAddress1.setBounds(25, 58, 96, 16);
-		card1.add(deliveryAddress1);
-
-		JLabel paymentLabel1 = new JLabel("Betalningss\u00E4tt");
-		paymentLabel1.setBounds(25, 280, 90, 15);
-		card1.add(paymentLabel1);
-
-		JButton abortButton = new JButton("Avbryt");
-		abortButton.setBounds(25, 490, 120, 55);
-		abortButton.setFont(new Font("Gill Sans", Font.PLAIN, 20));
-		abortButton.setHorizontalAlignment(SwingConstants.CENTER);
-		abortButton.setActionCommand("abort");
-		abortButton.addActionListener(myActionListener);
-		card1.add(abortButton);
-
-		JButton payButton = new JButton("Betala");
-		payButton.setBounds(465, 490, 140, 55);
-		payButton.setFont(new Font("Gill Sans", Font.PLAIN, 20));
-		payButton.setActionCommand("pay");
-		payButton.addActionListener(myActionListener);
-		card1.add(payButton);
-
-		JComboBox chooseDay1 = new JComboBox();
-		chooseDay1.setBounds(25, 235, 120, 25);
-		chooseDay1.setModel(dayModel);
-		card1.add(chooseDay1);
-
-		JComboBox chooseTime1 = new JComboBox();
-		chooseTime1.setBounds(150, 235, 90, 25);
-		chooseTime1.setModel(timeModel);
-		card1.add(chooseTime1);
-
-		JLabel expiresLabel1 = new JLabel("Utl\u00F6per:");
-		expiresLabel1.setBounds(25, 393, 56, 16);
-		card1.add(expiresLabel1);
-
-		JLabel headLabel1 = new JLabel("Redo att betala?");
-		headLabel1.setBounds(60, 5, 250, 35);
-		headLabel1.setHorizontalAlignment(SwingConstants.CENTER);
-		headLabel1.setForeground(Color.WHITE);
-		headLabel1.setFont(new Font("Gill Sans", Font.PLAIN, 28));
-		card1.add(headLabel1);
-
-		JLabel nameLabel = new JLabel(model.getCustomer().getFirstName() + " " + model.getCustomer().getLastName());
-		nameLabel.setBounds(24, 89, 138, 16);
-		nameLabel.setForeground(textColorLight);
-		card1.add(nameLabel);
-
-
-		JLabel addressLabel = new JLabel(model.getCustomer().getAddress());
-		addressLabel.setBounds(25, 117, 61, 16);
-		addressLabel.setForeground(textColorLight);
-		card1.add(addressLabel);
-
-		JLabel postAddressLabel = new JLabel(model.getCustomer().getPostCode() + 
-				" " + model.getCustomer().getPostAddress());
-		postAddressLabel.setBounds(25, 149, 61, 16);
-		postAddressLabel.setForeground(textColorLight);
-		card1.add(postAddressLabel);
-
-		JButton modifyButton = new JButton("€ndra");
-		modifyButton.addActionListener(myActionListener);
-		modifyButton.setActionCommand("change");
-		modifyButton.setBounds(150, 490, 160, 55);
-		modifyButton.setFont(new Font("Gill Sans", Font.PLAIN, 20));
-		modifyButton.setHorizontalAlignment(SwingConstants.CENTER);
-		card1.add(modifyButton);
-
 
 		JPanel card2 = new JPanel();
 		card2.setBackground(Constants.CONTRASTCOLOR.getColor());
@@ -176,25 +89,45 @@ public class CheckOutView extends JFrame {
 		nameTextField = new JTextField();
 		nameTextField.setBounds(25, 86, 155, 28);
 		nameTextField.setColumns(10);
+		if(model.getCustomer() != null){
+			nameTextField.setText(model.getCustomer().getFirstName() + " " + model.getCustomer().getLastName());
+		} else {
+			nameTextField.setText("-Fšrnamn och efternamn-");
+		}
 		card2.add(nameTextField);
 
 		addressTextField = new JTextField();
 		addressTextField.setBounds(25, 124, 248, 28);
 		addressTextField.setColumns(10);
+		if(model.getCustomer() != null){
+			nameTextField.setText(model.getCustomer().getAddress());
+		} else {
+			nameTextField.setText("-Adress-");
+		}
 		card2.add(addressTextField);
 
 		postCodeTextField = new JTextField();
 		postCodeTextField.setBounds(25, 157, 89, 28);
 		postCodeTextField.setColumns(10);
+		if(model.getCustomer() != null){
+			nameTextField.setText(model.getCustomer().getPostCode());
+		} else {
+			nameTextField.setText("-Postkod-");
+		}
 		card2.add(postCodeTextField);
 
 		cityTextField = new JTextField();
 		cityTextField.setBounds(118, 157, 155, 28);
 		cityTextField.setColumns(10);
+		if(model.getCustomer() != null){
+			nameTextField.setText(model.getCustomer().getPostAddress());
+		} else {
+			nameTextField.setText("-Postort-");
+		}
 		card2.add(cityTextField);
 
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(375, 20, 300, 455);
+		scrollPane.setBounds(372, 19, 300, 455);
 		card2.add(scrollPane);
 
 		JLabel deliveryDay = new JLabel("Leveransdag");
@@ -275,8 +208,9 @@ public class CheckOutView extends JFrame {
 		abortButton1.setFont(new Font("Gill Sans", Font.PLAIN, 20));
 		abortButton1.setHorizontalAlignment(SwingConstants.CENTER);
 		abortButton1.setActionCommand("abort");
-		abortButton.addActionListener(myActionListener);
 		card2.add(abortButton1);
+		
+		this.setLocation(300, 100);
 
 		this.setVisible(true);
 
