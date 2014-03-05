2367fbbf0891b4f21024ca8c62ea1282edae1f6c
Neon <e.olof.norlander@gmail.com>
2014-03-05 11:27:13 +0100

    IT SAVES THE CART OMGOMG

--- GUIProject/src/view/Main.java
+++ GUIProject/src/view/Main.java
@@ -9,7 +9,13 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+		    public void run() {
+		        Model.getInstance().shutDown();
+		    }
+		}));
 		Director start = new Director();
+		
 	}
 
 }
