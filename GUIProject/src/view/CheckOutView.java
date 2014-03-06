58a8b15ad2407cc2ab7756a391fa0d8b291e8f9b
Jesper Olsson <jeppzone@gmail.com>
2014-03-06 18:22:54 +0100

    checkoutviewyo

--- GUIProject/src/view/CheckOutView.java
+++ GUIProject/src/view/CheckOutView.java
@@ -45,8 +45,12 @@ public class CheckOutView extends JFrame {
 	private JTextField cityTextField;
 	private JTextField cardNumberTextField;
 	private JTextField cvcTextField;
+	private JTextField expiresMonth;
+	private JTextField expiresYear;
 	private CartController cartController;
 	private JPanel recieptItemsPanel;
+	private JButton changeButton;
+	private JComboBox chooseDay, chooseTime,cardType;
 	private CardLayout cardLayout = new CardLayout(0, 0);
 	private DefaultComboBoxModel dayModel = new DefaultComboBoxModel(
 			new String[] {"Måndag","Tisdag","Onsdag","Torsdag","Fredag","Lördag","Söndag"});
@@ -65,18 +69,29 @@ public class CheckOutView extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent evt) {
 				if(evt.getActionCommand().equals("pay")){
-					int reply = JOptionPane.showConfirmDialog(null, "Genomför detta köp?",
-							"Köp pågår..",
-							JOptionPane.OK_OPTION);
 					model.placeOrder(true);
 					cartController.emptyCart();
 					dispose();
 					JOptionPane.showMessageDialog(null,"Tack för att du handlar på iMat, Välkommen åter!", "Bekräftelse", 1);
-				} else if(evt.getActionCommand().equals("abort")){
-					dispose();
-				}
+				} 
+					else if(evt.getActionCommand().equals("abort")){
+						dispose();
+				} 
+					else if(evt.getActionCommand().equals("change")){
+						changeButton.setText("Spara");
+						changeButton.setActionCommand("save");
+						setEditability(true);
+						
+					}
+					
+					else if(evt.getActionCommand().equals("save")){
+						changeButton.setText("Ändra");
+						changeButton.setActionCommand("change");
+						setEditability(false);
+					}
 			}
 		};
+		
 		customer = model.getCustomer();
 		cartController = controller;
 		CheckOutView.items = items;
@@ -119,7 +134,7 @@ public class CheckOutView extends JFrame {
 		JButton payButton1 = new JButton("Betala " + String.format("%.1f", totalSum) + "kr");
 		payButton1.setFont(new Font("Gill Sans", Font.PLAIN, 20));
 		payButton1.setHorizontalAlignment(SwingConstants.CENTER);
-		payButton1.setBounds(435, 490, 192, 55);
+		payButton1.setBounds(435, 475, 192, 55);
 		payButton1.setActionCommand("pay");
 		payButton1.addActionListener(myActionListener);
 		card2.add(payButton1);;
@@ -132,7 +147,7 @@ public class CheckOutView extends JFrame {
 		card2.add(headLabel);
 
 		JButton abortButton1 = new JButton("Avbryt");
-		abortButton1.setBounds(87, 490, 192, 55);
+		abortButton1.setBounds(16, 475, 146, 55);
 		abortButton1.setFont(new Font("Gill Sans", Font.PLAIN, 20));
 		abortButton1.setHorizontalAlignment(SwingConstants.CENTER);
 		abortButton1.setActionCommand("abort");
@@ -149,31 +164,35 @@ public class CheckOutView extends JFrame {
 		expireDateLabel.setBounds(17, 120, 56, 16);
 		panel_2.add(expireDateLabel);
 
-		JTextField expiresMonth = new JTextField();
+		expiresMonth = new JTextField();
 		expiresMonth.setText(String.valueOf(model.getCreditCard().getValidYear()));
 		expiresMonth.setBounds(85, 114, 39, 27);
 		panel_2.add(expiresMonth);
+		expiresMonth.setEditable(false);
 
-		JTextField expiresYear = new JTextField();
+		expiresYear = new JTextField();
 		expiresYear.setText(String.valueOf(model.getCreditCard().getValidMonth()));
 		expiresYear.setBounds(123, 114, 41, 27);
 		panel_2.add(expiresYear);
+		expiresYear.setEditable(false);
 
 		cardNumberTextField = new JTextField();
 		cardNumberTextField.setText(model.getCreditCard().getCardNumber());
 		cardNumberTextField.setBounds(17, 80, 147, 28);
 		panel_2.add(cardNumberTextField);
 		cardNumberTextField.setColumns(10);
+		cardNumberTextField.setEditable(false);
 
 		cvcTextField = new JTextField();
 		cvcTextField.setText(String.valueOf(model.getCreditCard().getVerificationCode()));
 		cvcTextField.setBounds(162, 80, 39, 28);
 		panel_2.add(cvcTextField);
 		cvcTextField.setColumns(10);
+		cvcTextField.setEditable(false);
 
-		JComboBox cardType = new JComboBox();
-
+		cardType = new JComboBox();
 		cardType.setBounds(17, 41, 119, 27);
+		cardType.setEnabled(false);
 		panel_2.add(cardType);
 		cardType.setModel(new DefaultComboBoxModel(new String[] {"-Korttyp-","Visa","Mastercard"}));
 
@@ -182,6 +201,7 @@ public class CheckOutView extends JFrame {
 		}else{
 			cardType.setSelectedItem(cardType.getItemAt(2));
 		}
+		cardType.setEditable(false);
 
 		JLabel paymentLabel = new JLabel("Betalningssätt");
 		paymentLabel.setBounds(16, 14, 90, 15);
@@ -201,35 +221,22 @@ public class CheckOutView extends JFrame {
 		JLabel deliveryAddress = new JLabel("Leveransadress");
 		deliveryAddress.setBounds(19, 16, 96, 16);
 		panel.add(deliveryAddress);
+		
 		nameTextField = new JTextField();
 		nameTextField.setBounds(16, 46, 155, 28);
-		if(customer.getFirstName() != null && customer.getFirstName()!= null){
-			nameTextField.setText(customer.getFirstName() + " " + customer.getLastName());
-		} else {
-			nameTextField.setText("-Fï¿½rnamn och efternamn-");
-		}
+		nameTextField.setText(customer.getFirstName() + " " + customer.getLastName());
 		panel.add(nameTextField);
 		nameTextField.setBounds(16, 46, 155, 28);
 		nameTextField.setColumns(10);
+		panel.add(nameTextField);
+		nameTextField.setEditable(false);
 		
 		postCodeTextField = new JTextField();
 		postCodeTextField.setBounds(16, 109, 89, 28);
 		postCodeTextField.setColumns(10);
-//		if(customer.getPostCode()!=null){
-			postCodeTextField.setText(customer.getPostCode());
-//		}else{
-//			postCodeTextField.setText("'postkod'");
-//		}
+		postCodeTextField.setText(customer.getPostCode());
 		panel.add(postCodeTextField);
-	
-
-//
-//		if(customer != null){
-//			nameTextField.setText(customer.getFirstName() + " " + customer.getLastName());
-//		} else {
-//			nameTextField.setText("-Fï¿½rnamn och efternamn-");
-//		}
-			
+		postCodeTextField.setEditable(false);
 
 
 		cityTextField = new JTextField();
@@ -237,28 +244,14 @@ public class CheckOutView extends JFrame {
 		cityTextField.setColumns(10);
 		cityTextField.setText(customer.getPostAddress());
 		panel.add(cityTextField);
-
-	
-		
+		cityTextField.setEditable(false);
 
 		addressTextField = new JTextField();
 		addressTextField.setBounds(16, 79, 248, 28);
 		addressTextField.setColumns(10);
 		addressTextField.setText(customer.getAddress());
 		panel.add(addressTextField);
-
-		
-		
-
-
-		nameTextField.setBounds(16, 46, 155, 28);
-		if(customer.getFirstName() != null && customer.getFirstName()!= null){
-			nameTextField.setText(customer.getFirstName() + " " + customer.getLastName());
-		} else {
-			nameTextField.setText("-Förnamn och efternamn-");
-		}
-		panel.add(nameTextField);
-		nameTextField.setColumns(10);
+		addressTextField.setEditable(false);
 
 
 		JPanel panel_1 = new JPanel();
@@ -266,25 +259,48 @@ public class CheckOutView extends JFrame {
 		panel_3.add(panel_1);
 		panel_1.setLayout(null);
 
-		JComboBox chooseDay = new JComboBox();
+		chooseDay = new JComboBox();
 		chooseDay.setBounds(16, 43, 118, 27);
 		panel_1.add(chooseDay);
 		chooseDay.setModel(dayModel);
+		chooseDay.setEnabled(false);
 
-		JComboBox chooseTime = new JComboBox();
-	
+		chooseTime = new JComboBox();
 		chooseTime.setBounds(130, 44, 97, 25);
 		panel_1.add(chooseTime);
 		chooseTime.setModel(timeModel);
+		chooseTime.setEnabled(false);
 
 		JLabel deliveryDay = new JLabel("Leveransdag");
 		deliveryDay.setBounds(16, 16, 120, 15);
 		panel_1.add(deliveryDay);
+		
+		changeButton = new JButton("Ändra");
+		changeButton.setBounds(192, 475, 146, 55);
+		changeButton.setFont(new Font("Gill Sans", Font.PLAIN, 20));
+		changeButton.setHorizontalAlignment(SwingConstants.CENTER);
+		changeButton.setActionCommand("change");
+		changeButton.addActionListener(myActionListener);
+		card2.add(changeButton);
 
 		this.setLocation(300, 100);
 
 		this.setVisible(true);
 
 
+	}
+	
+	public void setEditability(boolean b){
+		nameTextField.setEditable(b);
+		addressTextField.setEditable(b);
+		postCodeTextField.setEditable(b);
+		cityTextField.setEditable(b);
+		cardNumberTextField.setEditable(b);
+		cvcTextField.setEditable(b);
+		chooseDay.setEnabled(b);
+		chooseTime.setEnabled(b);
+		cardType.setEnabled(b);
+		expiresMonth.setEditable(b);
+		expiresYear.setEditable(b);
 	}
 }
