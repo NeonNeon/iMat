980da8405bc0db82f0ff36b3171a812a97b3564c
lovisajaberg <lovis@Loviss-MacBook-Pro.local>
2014-03-14 17:37:09 +0100

    new header

--- GUIProject/src/view/StartView.java
+++ GUIProject/src/view/StartView.java
@@ -27,8 +27,9 @@ public class StartView extends JPanel {
 
 	private static final Model model = Model.getInstance();
 	FrameController frameController;
-	private Color background = Constants.BACKGROUNDCOLOR.getColor();
+	private Color background = Color.WHITE;
 	private Font textFont = Constants.TEXTFONT.getFont();
+	private Color textColor = Constants.TEXTCOLOR.getColor();
 	private Font headerFont = Constants.HEADERFONT.getFont();
 	private ImageIcon headerIcon = new ImageIcon("lib/Bildmapp/iMatHeader.jpg");
 	/**
@@ -42,38 +43,39 @@ public class StartView extends JPanel {
 		setLayout(null);
 		
 		JLabel logoLabel = new JLabel();
-		logoLabel.setBounds(10, 11, 664, 115);
+		logoLabel.setBounds(10, 11, 664, 185);
+		//logoLabel.setBorder(new LineBorder(Constants.TEXTCOLORLIGHT.getColor(),4));
 		logoLabel.setIcon(headerIcon);
 		add(logoLabel);
 		
 		JLabel nbrLabel1 = new JLabel("  1");
 		nbrLabel1.setFont(new Font("Tahoma", Font.BOLD, 16));
-		nbrLabel1.setBounds(246, 144, 35, 35);
+		nbrLabel1.setBounds(246, 200, 35, 35);
 		add(nbrLabel1);
 		
 		JLabel nbrLabel2 = new JLabel("  2");
 		nbrLabel2.setFont(new Font("Tahoma", Font.BOLD, 16));
-		nbrLabel2.setBounds(246, 178, 35, 35);
+		nbrLabel2.setBounds(246, 225, 35, 35);
 		add(nbrLabel2);
 		
 		JLabel nbrLabel3 = new JLabel("  3");
 		nbrLabel3.setFont(new Font("Tahoma", Font.BOLD, 16));
-		nbrLabel3.setBounds(246, 218, 35, 35);
+		nbrLabel3.setBounds(246, 250, 35, 35);
 		add(nbrLabel3);
 		
 		JLabel instructionLabel1 = new JLabel("V\u00E4lj dina varor");
 		instructionLabel1.setFont(textFont);
-		instructionLabel1.setBounds(291, 156, 127, 25);
+		instructionLabel1.setBounds(290, 205, 127, 25);
 		add(instructionLabel1);
 		
 		JLabel instructionLabel2 = new JLabel("Betala");
 		instructionLabel2.setFont(textFont);
-		instructionLabel2.setBounds(290, 190, 91, 25);
+		instructionLabel2.setBounds(290, 230, 91, 25);
 		add(instructionLabel2);
 		
 		JLabel instructionLabel3 = new JLabel("F\u00E5 maten hemk\u00F6rd!");
 		instructionLabel3.setFont(textFont);
-		instructionLabel3.setBounds(291, 230, 127, 25);
+		instructionLabel3.setBounds(290, 255, 127, 25);
 		add(instructionLabel3);
 		
 		JPanel offerPanel = new JPanel();
@@ -85,7 +87,8 @@ public class StartView extends JPanel {
 		
 		JLabel lblVeckansErbjudanden = new JLabel("Veckans erbjudanden!");
 		lblVeckansErbjudanden.setFont(headerFont);
-		lblVeckansErbjudanden.setBounds(222, 284, 246, 36);
+		lblVeckansErbjudanden.setForeground(Constants.TEXTCOLOR.getColor());
+		lblVeckansErbjudanden.setBounds(222, 290, 246, 36);
 		add(lblVeckansErbjudanden);
 		
 		//L�gger till erbjudanden l�ngst ner
